f0433544c7e3ed846ef73b5546aae29bf19c02bc
Rares Hanganu <rares.hanganur@gmail.com>
2024-06-09 14:26:17 +0300

    Make iele photos local

--- bot.py
+++ bot.py
@@ -12,20 +12,19 @@ ADMIN = 885074776467578900
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-poze_cu_iele = [
-    "https://www.fanatik.ro/wp-content/thumbnails/yfKwS-Gk2jg7ZnW7Z7qj7nXQuXY=/1200x0/smart/filters:contrast(5):format(jpeg):quality(80)/wp-content/uploads/2023/06/iele.jpg",
-    "https://i.pinimg.com/originals/3e/dd/d2/3eddd2d60457b71887d96a4bec8b59ac.jpg",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBkKYBAAlnLVtpdaU7hTBDXbiF_h0GxOWTHw&s",
-    "https://miro.medium.com/v2/resize:fit:1200/1*zlHUbLwIRnBkPPDCG_9cUQ.jpeg",
-    "https://i.ytimg.com/vi/tItT6uyE4lU/maxresdefault.jpg",
-    "https://miro.medium.com/v2/resize:fit:1400/1*hVhsJ8ZhOGQn3idWpoFt2w.jpeg"
-]
-
 correct_challenge = ""
 correct_trivia = ""
 
 @bot.command(name="iele", help="iti arata niste iele")
 async def iele(ctx):
+    poze_cu_iele = [
+        "https://www.fanatik.ro/wp-content/thumbnails/yfKwS-Gk2jg7ZnW7Z7qj7nXQuXY=/1200x0/smart/filters:contrast(5):format(jpeg):quality(80)/wp-content/uploads/2023/06/iele.jpg",
+        "https://i.pinimg.com/originals/3e/dd/d2/3eddd2d60457b71887d96a4bec8b59ac.jpg",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBkKYBAAlnLVtpdaU7hTBDXbiF_h0GxOWTHw&s",
+        "https://miro.medium.com/v2/resize:fit:1200/1*zlHUbLwIRnBkPPDCG_9cUQ.jpeg",
+        "https://i.ytimg.com/vi/tItT6uyE4lU/maxresdefault.jpg",
+        "https://miro.medium.com/v2/resize:fit:1400/1*hVhsJ8ZhOGQn3idWpoFt2w.jpeg"
+    ]
     await ctx.send(random.choice(poze_cu_iele))
 
 @bot.command(name="challenge", help="face un challenge")
