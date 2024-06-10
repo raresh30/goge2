622b3bcfc9b849a5bcce5d5aa4aab97894c97ee2
Rares Hanganu <rares.hanganur@gmail.com>
2024-06-10 20:21:12 +0300

    Clean things up

--- bot.py
+++ bot.py
@@ -10,14 +10,12 @@ import html
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHALLENGE_CHANNEL = 1192740543042682890
-ADMIN = 885074776467578900
 TRIVIA_API_URL = "https://opentdb.com/api.php?amount=1"
-
+BOT_PREFIX = "!"
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
 
-trivia_active = {} # Jucatorii activi de Trivia
+trivia_active = {} # jucatorii activi de trivia
 
 @bot.command(name="iele", help="iti arata niste iele")
 async def iele(ctx):
@@ -31,67 +29,65 @@ async def iele(ctx):
     ]
     await ctx.send(random.choice(poze_cu_iele))
 
-@bot.command(name="challenge", help="face un challenge")
-async def challenge(ctx, query, ans):
-    if ctx.author.id != ADMIN:
-        return
-    global correct_challenge
-    correct_challenge = str(ans)
-    channel = bot.get_channel(CHALLENGE_CHANNEL)
-    await channel.send(query)
-
-@bot.command(name="answer", help="sa dai raspunsul la challenge")
-async def answer(ctx, ans):
-    global correct_challenge
-    if ans != correct_challenge:
-        await ctx.send("WA")
-    else:
-        await ctx.send("AC")
-
-
 @bot.command(name="trivia", help="incepi un joc de trivia")
 async def trivia(ctx):
     global trivia_active
 
+    # daca cel care a apelat comanda este deja intr-un joc de trivia
     if ctx.author.id in trivia_active:
         await ctx.send("Ai deja un joc de trivia activ. Asteapta pana il termini.")
         return
 
+    # incercam sa luam o intrebare de trivia de la api
     response = requests.get(TRIVIA_API_URL)
-    if response.status_code != 200:
+    if response.status_code != 200: # codul returnat este diferit de succes?
         await ctx.send("Nu am putut prelua intrebarea de trivia momentan.")
         return
 
-    trivia_data = response.json()["results"][0]
-    question = html.unescape(trivia_data["question"])
-    correct_trivia = html.unescape(trivia_data["correct_answer"])
-    incorrect_trivia = [html.unescape(answer) for answer in trivia_data["incorrect_answers"]]
-    all_answers = incorrect_trivia + [correct_trivia]
-    random.shuffle(all_answers)
-
-    formatted_question = format_question(question)
-
-    await ctx.send(f"{ctx.author.mention}, intrebare: {formatted_question}")
-
-    for index, answer in enumerate(all_answers, start=1):
-        await ctx.send(f"{index}. {answer}")
-
+    # utilizatorul este acum intr-un joc de trivia
     trivia_active[ctx.author.id] = True
+
+    # in trivia_data avem toate informatiile returnate de api
+    trivia_data = response.json()["results"][0]
+
+    question = html.unescape(trivia_data["question"]) # intrebarea
+    correct_answer = html.unescape(trivia_data["correct_answer"]) # raspunsul corect
+    incorrect_answers = [] # raspunsurile incorecte
+    for ans in trivia_data["incorrect_answers"]: # pentru fiecare raspuns incorect
+        incorrect_answers.append(html.unescape(ans)) # adaugam in lista rapsunsul formatat
+    all_answers = incorrect_answers + [correct_answer] # toate raspunsurile
+    random.shuffle(all_answers) # reordonam aleator raspunsurile
+
+    # trimitem intrebarea pe discord
+    await ctx.send(f"{ctx.author.mention}, intrebare: {question}")
+
+    # vom parcurge toate raspunsurile valabile, pastrand si indicele
+    for index, answer in enumerate(all_answers, start=1):
+        await ctx.send(f"{index}. {answer}") # trimitem aceasta varianta de raspuns
     
+    # verifica daca mesajul curent este raspuns la trivia
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel \
+            and (not message.content.strip().startswith(BOT_PREFIX))
     try:
-        response = await bot.wait_for("message", check=lambda m: m.author == ctx.author and m.channel == ctx.channel, timeout=45.0)
+        # asteptam raspunsul jucatorului
+        response = await bot.wait_for("message", check=check, timeout=45.0)
     except asyncio.TimeoutError:
-        await ctx.send(f"{ctx.author.mention}, a expirat timpul! Raspunsul corect era: {correct_trivia}")
+        # jucatorul a ramas fara timp
+        await ctx.send(f"{ctx.author.mention}, "
+            f"a expirat timpul! Raspunsul corect era: {correct_trivia}")
     else:
-        if response.content.strip().lower() == correct_trivia.lower():
-            await ctx.send(f"{ctx.author.mention}, felicitari! Raspunsul tau a fost corect.")
-        elif response.content.strip().lower().startswith("!trivia") == False:
-            await ctx.send(f"{ctx.author.mention}, raspunsul tau este gresit. Raspunsul corect era: {correct_trivia}")
+        # am primit un raspuns
+        # raspuns corect
+        if response.content.strip().lower() == correct_answer.lower():
+            await ctx.send(f"{ctx.author.mention}, "
+                f"felicitari! Raspunsul tau a fost corect.")
+        # raspuns gresit
+        else:
+            await ctx.send(f"{ctx.author.mention}, "
+                f"raspunsul tau este gresit. Raspunsul corect era: {correct_answer}")
 
-    if ctx.author.id in trivia_active:
-        del trivia_active[ctx.author.id] 
-
-def format_question(question):
-    return question.replace("&quot;", '"').replace("&#039;", "'").replace("&amp;", "&").replace("&lt;", "<").replace("&gt;", ">")
+    # jucatorul a terminat jocul de trivia
+    del trivia_active[ctx.author.id]
 
 bot.run(TOKEN)
