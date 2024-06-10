004dbcab811a7ae1992cf78f05c70a777b5bd303
Rares Hanganu <rares.hanganur@gmail.com>
2024-06-10 21:02:27 +0300

    Add ban/unban commands

--- bot.py
+++ bot.py
@@ -7,31 +7,57 @@ import unidecode
 from discord.ext import commands
 from dotenv import load_dotenv
 import html
+import sqlite3
 
 load_dotenv()
+
 TOKEN = os.getenv("DISCORD_TOKEN")
 TRIVIA_API_URL = "https://opentdb.com/api.php?amount=1"
 BOT_PREFIX = "!"
+
+con = sqlite3.connect("db")
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
 
 trivia_active = {} # jucatorii activi de trivia
 
-@bot.command(name="iele", help="iti arata niste iele")
-async def iele(ctx):
-    poze_cu_iele = [
-        "https://www.fanatik.ro/wp-content/thumbnails/yfKwS-Gk2jg7ZnW7Z7qj7nXQuXY=/1200x0/smart/filters:contrast(5):format(jpeg):quality(80)/wp-content/uploads/2023/06/iele.jpg",
-        "https://i.pinimg.com/originals/3e/dd/d2/3eddd2d60457b71887d96a4bec8b59ac.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBkKYBAAlnLVtpdaU7hTBDXbiF_h0GxOWTHw&s",
-        "https://miro.medium.com/v2/resize:fit:1200/1*zlHUbLwIRnBkPPDCG_9cUQ.jpeg",
-        "https://i.ytimg.com/vi/tItT6uyE4lU/maxresdefault.jpg",
-        "https://miro.medium.com/v2/resize:fit:1400/1*hVhsJ8ZhOGQn3idWpoFt2w.jpeg"
-    ]
-    await ctx.send(random.choice(poze_cu_iele))
+# este user_id in baza de date?
+def is_in_database(user_id):
+    cursor = con.cursor()
+    # cauta user-ul in baza de date
+    res = cursor.execute(f"select * from users where discord_id={user_id};")
+    # am gasit ceva?
+    if res.fetchall():
+        return 1
+    # nu am gasit user-ul in baza de date
+    return 0
 
-@bot.command(name="trivia", help="incepi un joc de trivia")
+# adauga user_id in baza de date
+def add_user_to_database(user_id):
+    cursor = con.cursor()
+    cursor.execute(f"""
+        insert into users (discord_id, banned)
+        values ({user_id}, false);
+    """)
+    con.commit()
+
+# este user_id banat?
+def is_banned(user_id):
+    cursor = con.cursor()
+    res = cursor.execute(f"select * from users where discord_id = {user_id} and banned = true;")
+    if res.fetchall():
+        return 1
+    return 0
+
+@bot.command(name="trivia", help="joaca un meci de trivia")
 async def trivia(ctx):
     global trivia_active
+
+    # autorul comenzii este banat: iesire imediata
+    if is_banned(ctx.author.id):
+        await ctx.send("Esti banat. Daca consideri ca este o greseala, "
+            "vorbeste cu un administrator.")
+        return
 
     # daca cel care a apelat comanda este deja intr-un joc de trivia
     if ctx.author.id in trivia_active:
@@ -89,5 +115,35 @@ async def trivia(ctx):
 
     # jucatorul a terminat jocul de trivia
     del trivia_active[ctx.author.id]
+
+@bot.command(name="ban", help="baneaza pe cineva de la acest bot")
+async def ban(ctx, user):
+    # taie primele 2 si ultima litera din user, deoarece user-ul primit
+    # este de forma <@1249284551570624606>
+    user_id = user[2:-1]
+    if not is_in_database(user_id):
+        add_user_to_database(user_id)
+    cursor = con.cursor()
+    cursor.execute(f"""
+        update users
+        set banned = true
+        where discord_id={user_id};
+    """)
+    con.commit()
+
+@bot.command(name="unban", help="pardoneste pe cineva de la acest bot")
+async def unban(ctx, user):
+    # taie primele 2 si ultima litera din user, deoarece user-ul primit
+    # este de forma <@1249284551570624606>
+    user_id = user[2:-1]
+    if not is_in_database(user_id):
+        add_user_to_database(user_id)
+    cursor = con.cursor()
+    cursor.execute(f"""
+        update users
+        set banned = false
+        where discord_id={user_id};
+    """)
+    con.commit()
 
 bot.run(TOKEN)
